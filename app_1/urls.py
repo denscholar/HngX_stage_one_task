e2d8d25e05a321bb8d5d1cd03ede8083b6f9ef03
Dennis Akagha <d.akagha20@gmail.com>
2023-09-10 12:43:08 +0100

    add url correctly

--- app_1/urls.py
+++ app_1/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ProjectOneAPIView.as_view(), name='project')
+    path("api/", views.ProjectOneAPIView.as_view(), name='project')
 ]
